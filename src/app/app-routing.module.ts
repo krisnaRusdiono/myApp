78413f3e08ae8cdb9dea803ad96efae72233e257
Krisna Rusdiono <krisnarusdiono@gmail.com>
2024-01-26 11:45:09 +0700

    feat: create basic structure by adding commonly used feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'office', loadChildren: () => import('./modules/office/office.module').then(m => m.OfficeModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
