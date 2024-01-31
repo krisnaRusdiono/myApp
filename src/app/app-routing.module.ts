c68797049d8d9de07d33d5bc730650550fe9c25c
Krisna Rusdiono <krisnarusdiono@gmail.com>
2024-01-31 22:37:43 +0700

    feat: add notFound standalone components for handling unregistered url route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'office', loadChildren: () => import('./modules/office/office.module').then(m => m.OfficeModule) },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
