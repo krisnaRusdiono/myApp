7ee6ee881bc18743fcf17d3782673af64b4654c5
Krisna Rusdiono <krisnarusdiono@gmail.com>
2024-01-31 22:25:28 +0700

    feat: create components under "home" module and add it as components to handle "home" route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/components/home/home.component';
 
-const routes: Routes = [{ path: 'office', loadChildren: () => import('./modules/office/office.module').then(m => m.OfficeModule) }];
+const routes: Routes = [
+  { path: 'office', loadChildren: () => import('./modules/office/office.module').then(m => m.OfficeModule) },
+  { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
